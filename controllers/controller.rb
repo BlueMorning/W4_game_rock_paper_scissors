5c493518c8780c18efe6e47f0a588f27a86d134a
Eric Carles <carles.eric@wanadoo.fr>
2017-12-12 18:36:18 +0000

    Controller class, erb() methods added

--- controllers/controller.rb
+++ controllers/controller.rb
@@ -1,7 +1,8 @@
-require('sinatra')
+require("sinatra")
 require("sinatra/contrib/all") if development?()
 require("pry-byebug")
-
+require("./../models/game")
+require("./../models/player")
 
 
 
@@ -15,6 +16,9 @@ get('draw_your_weapon/round/:current_round/:nb_rounds/score/:player1/:player2') 
   nb_rounds       = params[:nb_round].to_i
   score_player_1  = params[:player1].to_i
   score_player_2  = params[:player2].to_i
+
+  @game = Game.new(current_round, nb_rounds, Player.new(score_player_1), Player.new(score_player_2))
+
 end
 
 # Route to the result page of the round
@@ -23,7 +27,18 @@ get('result_round/round/:current_round/:nb_rounds/score/:player1/:player2/weapon
   nb_rounds       = params[:nb_round].to_i
   score_player_1  = params[:player1].to_i
   score_player_2  = params[:player2].to_i
-  weapon          = params[:weapon]
+  weapon          = params[:weapon].to_i
+
+  @game = Game.new(current_round, nb_rounds, Player.new(score_player_1, weapon), Player.new(score_player_2))
+  @game.battle()
+
+  if(@game.is_game_over)
+    erb(:game_over)
+  else
+    erb(:game)
+  end
+
+
 end
 
 # Route to the final score
@@ -31,4 +46,7 @@ get('final_result/nb_rounds/:nb_rounds/final_score/:player1/:player2') do
   nb_rounds       = params[:nb_round].to_i
   score_player_1  = params[:player1].to_i
   score_player_2  = params[:player2].to_i
+
+  @game = Game.new(nb_rounds, nb_rounds, Player.new(score_player_1, weapon), Player.new(score_player_2))
+
 end
