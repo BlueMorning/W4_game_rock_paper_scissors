029c8fde635112d22d5dbd7dfd3fb65d4a082ce1
Eric Carles <carles.eric@wanadoo.fr>
2017-12-12 17:53:41 +0000

    Actions routing completed

--- controllers/controller.rb
+++ controllers/controller.rb
@@ -9,7 +9,26 @@ get('/homepage') do
 
 end
 
+# Routing to the weapon choice for the round
+get('draw_your_weapon/round/:current_round/:nb_rounds/score/:player1/:player2') do
+  current_round   = params[:current_round].to_i
+  nb_rounds       = params[:nb_round].to_i
+  score_player_1  = params[:player1].to_i
+  score_player_2  = params[:player2].to_i
+end
 
-get('/start_game') do
+# Route to the result page of the round
+get('result_round/round/:current_round/:nb_rounds/score/:player1/:player2/weapon/:weapon') do
+  current_round   = params[:current_round].to_i
+  nb_rounds       = params[:nb_round].to_i
+  score_player_1  = params[:player1].to_i
+  score_player_2  = params[:player2].to_i
+  weapon          = params[:weapon]
+end
 
+# Route to the final score
+get('final_result/nb_rounds/:nb_rounds/final_score/:player1/:player2') do
+  nb_rounds       = params[:nb_round].to_i
+  score_player_1  = params[:player1].to_i
+  score_player_2  = params[:player2].to_i
 end
